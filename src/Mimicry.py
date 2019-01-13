0bc6ea5bbbcfa37f73c7d07745eb85260ca4cb36
egilligan <eg207@st-andrews.ac.uk>
2019-01-13 14:20:50 +0000

    Initial Commit

---
+++ src/Mimicry.py
@@ -0,0 +1,21 @@
+import discord
+import json;
+
+with open("credentials.json") as credentialsFile:
+    credentials = json.load(credentialsFile)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Squawk")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Hello":
+        await client.send_message(message.channel, "Squawk")
+
+client.run(credentials["token"])
+
