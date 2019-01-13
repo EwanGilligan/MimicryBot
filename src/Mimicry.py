7a6c68802d1ea65626ccd70b6d49c553517a56c4
egilligan <eg207@st-andrews.ac.uk>
2019-01-13 22:20:08 +0000

    Implemented mimic command, with regex checking

--- src/Mimicry.py
+++ src/Mimicry.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import generator
+import re
 
 with open("credentials.json") as credentialsFile:
     credentials = json.load(credentialsFile)
@@ -23,7 +24,9 @@ async def on_message(message):
         await client.send_message(message.channel, "Setting up")
         await read_message_history(message.server)
     if message.content.startswith("!mimic"):
-        await generate_sentences("104313625824481280")
+        await mimic(message)
+
+
 
 
 async def read_message_history(server):
@@ -45,6 +48,15 @@ async def write_message(message):
 async def generate_sentences(ID):
     filePath = credentials["data_path"] + '/' + ID
     generator.build_model(filePath)
+
+async def mimic(message):
+    #Checks first that the message is of the correct format.
+    if re.search("!mimic [0-9]{18}", message.content):
+        #Generates the sentence with the chosen id.
+        await generate_sentences(str(re.search("[0-9]{18}", message.content).group()))
+    else:
+        await client.send_message(message.channel, "Invalid syntax")
+
 
 
 
