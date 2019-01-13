0b7acde9b634792ad1c2570bc47403c5b6a0d294
egilligan <eg207@st-andrews.ac.uk>
2019-01-13 21:50:26 +0000

    Fixed issue with the bot reading messages from all possible channels, not specific to server

--- src/Mimicry.py
+++ src/Mimicry.py
@@ -4,12 +4,13 @@ import json
 with open("credentials.json") as credentialsFile:
     credentials = json.load(credentialsFile)
 
-
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print("Squawk")
+
 
 @client.event
 async def on_message(message):
@@ -19,15 +20,15 @@ async def on_message(message):
         await client.send_message(message.channel, "Squawk")
     if message.content.startswith("!mimicry_setup"):
         await client.send_message(message.channel, "Setting up")
-        await read_message_history()
+        await read_message_history(message.server)
 
 
-async def read_message_history():
-    #Gets all channels the client has access to
-    for channel in client.get_all_channels():
-        #Gets all the messages from each client.
+async def read_message_history(server):
+    # Gets all channels the client has access to
+    for channel in server.channels:
+        # Gets all the messages from each client.
         async for message in client.logs_from(channel, limit=100):
-            #Writes the messages to the file listed by the users name
+            # Writes the messages to the file listed by the users name
             if message.author != client.user:
                 print(message.content)
                 await write_message(message)
@@ -40,4 +41,3 @@ async def write_message(message):
 
 
 client.run(credentials["token"])
-
