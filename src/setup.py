aad65e6a0dfdd7fe74988215c3cf488ce23bddc4
egilligan <eg207@st-andrews.ac.uk>
2019-01-14 21:51:24 +0000

    Fixed setup file

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name='MimicryBot',
+setup(name='mimicrybot',
       version='0.1',
       description='A discord bot that can generate a random sentence for a user, based on their previous messages.',
       url='https://github.com/EwanGilligan/MimicryBot',
       author='Ewan Gilligan',
       author_email='eg207@st-andrews.ac.uk',
       license='MIT',
-      packages=['generator', 'mimicry'],
+      packages=find_packages(),
       install_requires=[
           'discord',
           'markovify',
